e66b24f807a2a77dd57d0d71cbe5432c8e43bf43
DenisBilokon <106659593+DenisBilokon@users.noreply.github.com>
2022-10-07 23:20:35 +0300

    Add files via upload

---
+++ 4.3.cs
@@ -0,0 +1,58 @@
+﻿
+namespace Program;
+
+public class Invoice
+{
+    protected string _receiver;
+    public string Receiver
+    {
+        get
+        {
+            return _receiver;
+        }
+    }
+
+    protected int _price;
+    public int Price
+    {
+        get
+        {
+            return _price;
+        }
+    }
+
+    protected DateTime _date = DateTime.Now;
+    public DateTime Date
+    {
+        get
+        {
+            return _date;
+        }
+    }
+
+    public Invoice(string receiver, int price, DateTime date)
+    {
+        _receiver = receiver;
+        _price = price;
+        _date = date;
+    }
+}
+
+
+public abstract class InvoiceDebugger : Invoice
+{
+    public InvoiceDebugger(string receiver, int price, DateTime date) : base(receiver, price, date) { }
+
+    virtual public void DebugValue() { }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var receiver = "Andrii";
+        var price = 25;
+        var date = DateTime.Now;
+    }
+}
+
---
+++ 4.4.cs
@@ -0,0 +1,90 @@
+﻿namespace Program;
+
+public class ApplicationLicense
+{
+    public enum AccessLevels : int
+    {
+        Free = 1,
+        Trial = 2,
+        Pro = 3
+    }
+
+    private string? _key;
+    private Nullable<int> _accessLevel;
+
+    public int AccessLevel
+    {
+        get
+        {
+            var accessLevel = _accessLevel;
+
+            if (!accessLevel.HasValue)
+            {
+                accessLevel = _accessLevel = ReadKey();
+            }
+
+            return (int)accessLevel;
+        }
+    }
+
+    public ApplicationLicense(string? key)
+    {
+        _key = key;
+    }
+
+    private int ReadKey()
+    {
+        switch (_key)
+        {
+            case "pro":
+                return (int)AccessLevels.Pro;
+            case "trial":
+                return (int)AccessLevels.Trial;
+            default:
+                return (int)AccessLevels.Free;
+        }
+    }
+
+    public void PrintLicense()
+    {
+        switch (AccessLevel)
+        {
+            case (int)AccessLevels.Free:
+                Console.WriteLine("Безкоштовна версiя");
+                break;
+            case (int)AccessLevels.Trial:
+                Console.WriteLine("Пробна версiя");
+                break;
+            case (int)AccessLevels.Pro:
+                Console.WriteLine("Про версiя");
+                break;
+        }
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введiть лiцензiйний ключ: ");
+        var key = Console.ReadLine();
+
+        var license = new ApplicationLicense(key);
+
+        if (license.AccessLevel >= (int)ApplicationLicense.AccessLevels.Pro)
+        {
+            Console.WriteLine("Функцiя Pro доступна");
+        }
+
+        if (license.AccessLevel >= (int)ApplicationLicense.AccessLevels.Trial)
+        {
+            Console.WriteLine("Функцiя Пробна доступна");
+        }
+
+        if (license.AccessLevel >= (int)ApplicationLicense.AccessLevels.Free)
+        {
+            Console.WriteLine("Безкоштовна Функцiя  доступна");
+        }
+    }
+}
